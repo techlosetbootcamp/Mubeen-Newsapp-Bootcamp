9279e2b4da71f5f2c014e491174a1f4c6a6a7e91
Mubeen-creator <mmaj8855@gmail.com>
2025-01-03 17:20:26 +0500

    Fixed Review Code

--- src/components/header/useHeader.ts
+++ src/components/header/useHeader.ts
@@ -4,22 +4,10 @@ import { useNavigate } from "react-router-dom";
 const useHeader = () => {
   const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState("Latest Stories");
-  const handleTabClick = (tab: string) => {
-    setActiveTab(tab);
-
-    if (tab === "Latest Stories") {
-      navigate("/");
-    } else if (tab === "Opinion") {
-      navigate("/podcasts");
-    } else if (tab === "Health") {
-      navigate("/corona-updates");
-    }
-  };
   return {
     navigate,
     activeTab,
     setActiveTab,
-    handleTabClick,
   };
 };
 
